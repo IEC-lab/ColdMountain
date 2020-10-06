f26370a80b977de0c70957c315a5571b03262b3f
unknown <770340084@qq.com>
2020-10-06 11:36:05 +0800

    support graphql

--- graphql/graph/schema.resolvers.go
+++ graphql/graph/schema.resolvers.go
@@ -5,24 +5,15 @@ package graph
 
 import (
 	"ColdMountain/graphql/graph/generated"
-	"ColdMountain/graphql/graph/model"
+	"ColdMountain/streams"
 	"context"
-	"fmt"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *queryResolver) GetFrameStreams(ctx context.Context) ([]string, error) {
+	return streams.DiscoverStreams(), nil
 }
-
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"ColdMountain/graphql/adaptation"
 	"ColdMountain/middlewares"
-	"ColdMountain/streams"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,15 +16,8 @@ func main(){
 				"message": "pong",
 			})
 		})
-		apiR.GET("/streams", GetStreams)
 		apiR.GET("/graphql/playground", adaptation.PlaygroundHandler())
 		apiR.POST("/graphql/query", adaptation.GraphqlHandler())
 	}
 	_ = r.Run(":8899")
-}
-
-func GetStreams(context *gin.Context){
-	context.JSON(200, gin.H{
-		"streams": streams.DiscoverStreams(),
-	})
 }
